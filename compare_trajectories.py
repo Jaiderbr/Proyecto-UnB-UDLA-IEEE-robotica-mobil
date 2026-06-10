a19bf2a3771b7dae815ca6e1e26b0a0920ec6493
Jaider Bautista Rodriguez <jaid.bautista@udla.edu.co>
2026-06-09 21:01:18 -0500

    ㅤㅤ

--- compare_trajectories.py
+++ compare_trajectories.py
@@ -170,8 +170,8 @@ def load_csv_auto(path: Path) -> Tuple[List[float], List[float], List[float]]:
     
     return times, xs, ys
 
-REAL_ROOT = Path(r"D:\Proyecto-UnB-UDLA-IEEE-robotica-mobil\20260609_144552")
-COPPELIA_ROOT = Path(r"D:\Proyecto-UnB-UDLA-IEEE-robotica-mobil\CoppeliaEsp\Cars_Trajectory_V3\20260609_144552")
+REAL_ROOT = Path(r"C:\Users\jaide\Desktop\Proyecto-UnB-UDLA-IEEE-rob-tica-mobil\CoppeliaEsp\Cars_Trajectory_V3\20260609_152552")
+COPPELIA_ROOT = Path(r"C:\Users\jaide\Desktop\Proyecto-UnB-UDLA-IEEE-rob-tica-mobil\20260609_152552")
 
 def main() -> None:
     real_root = REAL_ROOT
